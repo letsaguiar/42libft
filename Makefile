fd8e77502c4c09e5de5fc6b8dfe229276930ec39
Letícia Aguiar <letsaguiar@yandex.com>
2024-04-21 09:19:21 -0300

    feat: remove some functions

--- Makefile
+++ Makefile
@@ -2,36 +2,7 @@ CC := cc
 CFLAGS := -Wall -Wextra -Werror
 
 SRC_HEADERS := src
-SRC_FILES := \
-	ft_islower.c \
-	ft_isupper.c \
-	ft_isalpha.c \
-	ft_isdigit.c \
-	ft_isalnum.c \
-	ft_isascii.c \
-	ft_isprint.c \
-	ft_isspace.c \
-	ft_strlen.c \
-	ft_strlcpy.c \
-	ft_strlcat.c \
-	ft_strchr.c \
-	ft_strrchr.c \
-	ft_strncmp.c \
-	ft_strnstr.c \
-	ft_strdup.c \
-	ft_bzero.c \
-	ft_memset.c \
-	ft_memcpy.c \
-	ft_memmove.c \
-	ft_memchr.c \
-	ft_memcmp.c \
-	ft_toupper.c \
-	ft_tolower.c \
-	ft_atoi.c \
-	ft_calloc.c \
-	ft_substr.c \
-	ft_strjoin.c \
-	ft_strtrim.c
+SRC_FILES :=
 OBJ_FILES := $(subst .c,.o,$(SRC_FILES))
 
 TARGET := libft.a
--- libft.h
+++ libft.h
@@ -6,70 +6,12 @@
 /*   By: letsaguiar <letsaguiar@yandex.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 21:55:31 by letsaguiar        #+#    #+#             */
-/*   Updated: 2024/04/18 13:25:20 by letsaguiar       ###   ########.fr       */
+/*   Updated: 2024/04/21 09:20:02 by letsaguiar       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdlib.h>
-
-int             ft_islower(int c);
-
-int             ft_isupper(int c);
-
-int				ft_isalpha(int c);
-
-int				ft_isdigit(int c);
-
-int				ft_isalnum(int c);
-
-int				ft_isascii(int c);
-
-int				ft_isprint(int c);
-
-int             ft_isspace(int c);
-
-size_t          ft_strlen(const char *str);
-
-size_t          ft_strlcpy(char *dest, const char *src, size_t n);
-
-size_t          ft_strlcat(char *dest, const char *src, size_t n);
-
-char            *ft_strchr(const char *str, int c);
-
-char            *ft_strrchr(const char *str, int c);
-
-int             ft_strncmp(const char *s1, const char *s2, size_t n);
-
-char            *ft_strnstr(const char *s1, const char *s2, size_t n);
-
-char            *ft_strdup(const char *s1);
-
-void            ft_bzero(void *str, size_t n);
-
-void            *ft_memset(void *str, int c, size_t n);
-
-void            *ft_memcpy(void *dest, const void *src, size_t n);
-
-void            *ft_memmove(void *dest, const void *src, size_t n);
-
-void            *ft_memchr(void *str, int c, size_t n);
-
-int             ft_memcmp(const void *s1, const void *s2, size_t n);
-
-int             ft_toupper(int c);
-
-int             ft_tolower(int c);
-
-int             ft_atoi(const char *str);
-
-void            *ft_calloc(size_t count, size_t size);
-
-char            *ft_substr(const char *str, size_t start, size_t len);
-
-char            *ft_strjoin(const char *s1, const char *s2);
-
-char            *ft_strtrim(const char *s1, const char *set);
 
 #endif
