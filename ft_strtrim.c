6e4c271126ddb0086c73d7e1e17b94f6c14496b8
Letícia Aguiar <leticia_silva@trimble.com>
2024-04-22 13:58:59 -0300

    feat: update strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,18 +6,16 @@
 /*   By: letsaguiar <letsaguiar@yandex.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:37:25 by letsaguiar        #+#    #+#             */
-/*   Updated: 2024/04/22 13:46:57 by letsaguiar       ###   ########.fr       */
+/*   Updated: 2024/04/22 13:58:15 by letsaguiar       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// aaHello Worldbb 2 13
 char    *ft_strtrim(const char *s, const char *set)
 {
     size_t  start;
     size_t  end;
-    char    *ptr;
 
     if (!s)
         return (ft_strdup(""));
@@ -29,11 +27,5 @@ char    *ft_strtrim(const char *s, const char *set)
     end = ft_strlen(s);
     while (s[end - 1] && ft_strchr(set, s[end - 1]))
         end--;
-    if (start >= end)
-        return (ft_strdup(""));
-    ptr = (char *) malloc((end - start + 1) * sizeof (char));
-    if (!ptr)
-        return (NULL);
-    ft_strlcpy(ptr, s + start, (end - start + 1));
-    return (ptr);
+    return (ft_substr(s, start, end - start));
 }
